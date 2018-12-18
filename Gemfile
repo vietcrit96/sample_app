c721fd5159bee0bb60611ec889a1720528c842a5
vietcrit96 <vietcrit@gmail.com>
2018-12-18 14:10:39 +0700

    Chapter 11: Account activation

--- Gemfile
+++ Gemfile
@@ -4,12 +4,15 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 ruby "2.5.1"
 gem "bcrypt", "3.1.12"
 gem "bootsnap", ">= 1.1.0", require: false
+gem "bootstrap-kaminari-views", "~> 0.0.5"
 gem "bootstrap-sass", "3.3.7"
 gem "coffee-rails", "~> 4.2"
 gem "config"
 gem "faker", "~> 1.9", ">= 1.9.1"
+gem "figaro", "~> 1.1", ">= 1.1.1"
 gem "jbuilder", "~> 2.5"
 gem "jquery-rails", "~> 4.3", ">= 4.3.3"
+gem "kaminari", "~> 1.1", ">= 1.1.1"
 gem "puma", "~> 3.11"
 gem "rails", "~> 5.2.2"
 gem "rails-i18n"
@@ -17,8 +20,6 @@ gem "sass-rails", "~> 5.0"
 gem "sqlite3"
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
-gem "kaminari", "~> 1.1", ">= 1.1.1"
-gem "bootstrap-kaminari-views", "~> 0.0.5"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_reader :remember_token, :activation_token
+  before_create :create_activation_digest
+  before_save :downcase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true,
     length: {maximum: Settings.user_name_length_max}
@@ -8,8 +10,8 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true,
     length: {minimum: Settings.user_pass_length_min}, allow_nil: true
-  before_save{email.downcase!}
   has_secure_password
+  scope :activated, -> {where activated: true}
 
   class << self
     def digest string
@@ -26,9 +28,10 @@ class User < ApplicationRecord
     end
   end
 
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated? attribute, remember_token
+    digest = send "#{attribute}_digest"
+    return unless !digest.nil?
+    BCrypt::Password.new(digest).is_password? remember_token
   end
 
   def forget
@@ -42,5 +45,23 @@ class User < ApplicationRecord
 
   def current_user? current_user
     self == current_user
+  end
+  
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    @activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
