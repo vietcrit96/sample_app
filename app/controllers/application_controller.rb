159c0d86288a5a6d3c38c4b4f3778308f511b1f6
vietcrit96 <vietcrit@gmail.com>
2018-12-18 14:10:39 +0700

    Chapter 4: Rails-flavored Ruby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,4 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-
-  def set_locale
-    locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-    locale : I18n.default_locale
-  end
 end
